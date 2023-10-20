a089fa23786e63464d9fea6b55c427b5e912cee9
Qavi <qavi.shaikh11@gmail.com>
2023-10-20 11:50:37 +0500

    Comments $ Class

--- first.php
+++ first.php
@@ -17,4 +17,35 @@
     $name = "Qavi";
     echo"How are You : ".$name."<br>";
     var_dump($name);
+
+    $arr1 = array("Qavi","Hamdan","Hassan");
+    var_dump($arr1);
+
+    echo"<br>";
+    class Bike{
+        public $color;
+        public $model;
+        public function __construct($color,$model){
+            $this->color = $color;
+            $this->model = $model;
+        }
+        public function message(){
+            return "My Car is ". $this->color . " " . "Model is " .  $this->model . "!";
+        }
+    }
+
+    $myBike = new Bike("Red","Honda");
+    echo $myBike -> message();
+    echo"<br>";
+    $myBike = new Bike("Black","MG");
+    echo $myBike -> message();
+
+
+    // echo "\n"; it add a new line
+    # Single line comment
+    // Single line comment
+    /*
+    Muti line 
+    Comment
+    */
 ?>
