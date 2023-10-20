94c756b10cb73f83261a7a864e93aad7b009babf
Qavi <qavi.shaikh11@gmail.com>
2023-10-20 11:09:55 +0500

    data types

--- first.php
+++ first.php
@@ -1,3 +1,20 @@
 <?php
-    echo"<b>Qavi Shaikh</b>"
+    echo"First Practice";
+    echo"<br>";
+    echo"<b>Qavi</b> <h1>Shaikh</h1>";
+    echo 786;
+
+    print"<br>";
+    print"Qavi Shaikh";
+
+
+    $A = "<br> Ali Hasan ";
+    echo"$A";
+
+    $age = 20;
+    echo"<h1>".$age."</h1>";
+
+    $name = "Qavi";
+    echo"How are You : ".$name."<br>";
+    var_dump($name);
 ?>
